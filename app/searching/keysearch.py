32dc62ff249081e719fc365996718bbb487bbdf3
Tran Cong Minh <tc.tranminh@gmail.com>
2023-12-17 07:40:59 +0700

    ok'

--- app/searching/keysearch.py
+++ app/searching/keysearch.py
@@ -8,7 +8,6 @@ def keysearch():
     user_input = get_input()
 
     # Translate the user input to English
-    translated_input = response.generations[0].text
     response2 = co.generate(
         prompt='Based on the web search results from https://thegioididong.com and https://anphatpc.com.vn, Only give concise keywords about model, specifications of cheap laptop in Vietnamese Dong related to the following sentence ' + user_input + 'only from from https://thegioididong.com and https://anphatpc.com.vn. Response have only concise specifications, no other question',
     )
