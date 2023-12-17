5926246108ab813e4e711aa103f27a3dd8d72da0
Tran Cong Minh <tc.tranminh@gmail.com>
2023-12-17 07:43:05 +0700

    ok'

--- app/searching/embedded_search.py
+++ app/searching/embedded_search.py
@@ -17,36 +17,35 @@ model_name = "embed-multilingual-v3.0"
 input_type_embed = "search_document"
 
 def get_selected_fields():
-    search_key = get_search_key()
+    # search_key = get_search_key()
     results = get_results()
 
-    df_results = pd.DataFrame(results)
-    df_results['combined_text'] = df_results['config'].apply(lambda x: ' '.join(map(str, x))) + ' ' + df_results[
-        'name'] + ' ' + df_results['price'].astype(str)
+    # df_results = pd.DataFrame(results)
+    # df_results['combined_text'] = df_results['config'].apply(lambda x: ' '.join(map(str, x))) + ' ' + df_results[
+    #     'name'] + ' ' + df_results['price'].astype(str)
 
-    embeds_combined = co.embed(texts=list(df_results['combined_text']), model=model_name,
-                               input_type=input_type_embed).embeddings
+    # embeds_combined = co.embed(texts=list(df_results['combined_text']), model=model_name,
+    #                            input_type=input_type_embed).embeddings
 
-    search_index_combined = AnnoyIndex(np.array(embeds_combined).shape[1], 'angular')
-    for i in range(len(embeds_combined)):
-        search_index_combined.add_item(i, embeds_combined[i])
-    search_index_combined.build(10)
-    search_index_combined.save('test_combined.ann')
+    # search_index_combined = AnnoyIndex(np.array(embeds_combined).shape[1], 'angular')
+    # for i in range(len(embeds_combined)):
+    #     search_index_combined.add_item(i, embeds_combined[i])
+    # search_index_combined.build(10)
+    # search_index_combined.save('test_combined.ann')
 
-    def get_nearest_neighbors_for_combined(query):
-        input_type_query = "search_query"
-        query_embed = co.embed(texts=[query], model=model_name, input_type=input_type_query).embeddings
-        similar_item_ids = search_index_combined.get_nns_by_vector(query_embed[0], 5, include_distances=False)
-        query_results = df_results.iloc[similar_item_ids].copy()
-        return query_results
+    # def get_nearest_neighbors_for_combined(query):
+    #     input_type_query = "search_query"
+    #     query_embed = co.embed(texts=[query], model=model_name, input_type=input_type_query).embeddings
+    #     similar_item_ids = search_index_combined.get_nns_by_vector(query_embed[0], 5, include_distances=False)
+    #     query_results = df_results.iloc[similar_item_ids].copy()
+    #     return query_results
 
-    pd.set_option('display.max_columns', None)
+    # pd.set_option('display.max_columns', None)
 
-    query = search_key
-    #combined_nearest_neighbors_result = get_nearest_neighbors_for_combined(query)
+    # query = search_key
+    # #combined_nearest_neighbors_result = get_nearest_neighbors_for_combined(query)
 
-    selected_fields = results
     # Convert the DataFrame to a JSON serializable format
-    selected_fields_json = selected_fields.to_dict(orient='records')
+    # selected_fields_json = selected_fields.to_dict(orient='records')
 
-    return selected_fields_json
+    return results
